50252cd39dd71d729217bc913539c315104080f5
Webber <webber@takken.io>
2020-09-28 19:35:50 +0200

    Add test cases for onlyPrefix and exceptPrefix

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -34,4 +34,76 @@ describe('shouldDelete', () => {
     }
     expect(shouldDelete(expiredArtifact as any, actionInptus)).toEqual(true)
   })
+  test('should delete when matched by onlyPrefix', () => {
+    const expiredArtifact = {
+      created_at: new Date(),
+      name: 'tmp_artifact.test'
+    }
+    const actionInptus: IActionInputs = {
+      expireInMs: 0,
+      onlyPrefix: 'tmp',
+      exceptPrefix: ''
+    }
+    expect(shouldDelete(expiredArtifact as any, actionInptus)).toEqual(true)
+  })
+  test('should not delete when not matched by onlyPrefix', () => {
+    const expiredArtifact = {
+      created_at: new Date(),
+      name: 'build_artifact.test'
+    }
+    const actionInptus: IActionInputs = {
+      expireInMs: 0,
+      onlyPrefix: 'tmp',
+      exceptPrefix: ''
+    }
+    expect(shouldDelete(expiredArtifact as any, actionInptus)).toEqual(false)
+  })
+  test('should delete when not matched by exceptPrefix', () => {
+    const expiredArtifact = {
+      created_at: new Date(),
+      name: 'tmp_artifact.test'
+    }
+    const actionInptus: IActionInputs = {
+      expireInMs: 0,
+      onlyPrefix: '',
+      exceptPrefix: 'master_'
+    }
+    expect(shouldDelete(expiredArtifact as any, actionInptus)).toEqual(true)
+  })
+  test('should not delete when matched by exceptPrefix', () => {
+    const expiredArtifact = {
+      created_at: new Date(),
+      name: 'master_artifact.test'
+    }
+    const actionInptus: IActionInputs = {
+      expireInMs: 0,
+      onlyPrefix: '',
+      exceptPrefix: 'master_'
+    }
+    expect(shouldDelete(expiredArtifact as any, actionInptus)).toEqual(false)
+  })
+  test('should not delete when matched by both onlyPrefix and exceptPrefix', () => {
+    const expiredArtifact = {
+      created_at: new Date(),
+      name: 'master_tmp_artifact.test'
+    }
+    const actionInptus: IActionInputs = {
+      expireInMs: 0,
+      onlyPrefix: 'master_',
+      exceptPrefix: 'master_tmp_'
+    }
+    expect(shouldDelete(expiredArtifact as any, actionInptus)).toEqual(false)
+  })
+  test('should delete when matched by onlyPrefix but not exceptPrefix', () => {
+    const expiredArtifact = {
+      created_at: new Date(),
+      name: 'master_tmp_artifact.test'
+    }
+    const actionInptus: IActionInputs = {
+      expireInMs: 0,
+      onlyPrefix: 'master_',
+      exceptPrefix: 'tmp_'
+    }
+    expect(shouldDelete(expiredArtifact as any, actionInptus)).toEqual(true)
+  })
 })
